fc2645bd5031dacbaa0ece6087f2fe9bc9aa48d4
MatiasAguero <MatiasAguero@github.com>
2019-07-08 19:32:48 -0300

    Administracion de carreras arreglado

--- src/app/admin/carrera/carrera.component.ts
+++ src/app/admin/carrera/carrera.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Carrera } from '../../model/carrera';
+import { CarreraService } from '../../services/carrera.service';
 import { CarreraFormComponent } from './carrera-form/carrera-form.component';
+
 
 @Component({
   selector: 'app-carrera',
@@ -8,11 +11,26 @@ import { CarreraFormComponent } from './carrera-form/carrera-form.component';
   styleUrls: ['./carrera.component.scss']
 })
 export class CarreraComponent implements OnInit {
+  carreras: Carrera[];
+  selectedCarrera: String;
 
-  constructor() {}
+  constructor(
+    private carreraService: CarreraService
+  ) {}
 
   ngOnInit() {
-    
+    this.getAll()
   }
 
+  getAll() : void{
+    this.carreraService.getCarreras().subscribe(carreras => this.carreras = carreras);
+  }
+
+  updateCarrera(carrera : Carrera): void {
+    this.selectedCarrera=carrera.nombre;
+  }
+
+  delCarrera(nombre : String): void {
+    this.carreraService.deleteCarrera(nombre);
+  }
 }
